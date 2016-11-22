178dc79e07b2822314baeba61c998ddb53d929e0
SophieProud <rmmindru1@sheffield.ac.uk>
2016-11-22 00:08:39 +0000

    check

---
+++ Haiku Heart/functions.js
@@ -0,0 +1,17 @@
+function myFunction(x){
+	x.classList.toggle("change");
+}
+
+$(document).ready(function() {
+    	
+			$('ul a').click(function() {
+			var $anchor = $(this);
+			
+			$('html, body').animate({
+			scrollTop: $($anchor.attr('href')).offset().top
+			}, 2000);
+			return false;
+			helpers : {
+			}
+			});
+			});
